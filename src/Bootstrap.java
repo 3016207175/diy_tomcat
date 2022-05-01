8a092322d7cd2104fb53c2e6be108c9ff69fd7ef
baoziming <514937651@qq.com>
2022-05-01 17:06:45 +0800

    init

---
+++ src/Bootstrap.java
@@ -0,0 +1,13 @@
+
+import connector.Connector;
+
+/**
+ * @author BaoZiming
+ * @create 2022-05-01 15:00
+ */
+public final class Bootstrap {
+
+    public static void main(String[] args) {
+        new Connector().start();
+    }
+}
